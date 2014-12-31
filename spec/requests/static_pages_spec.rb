3893e7be9e6a3591dd4363014f579c9b623d0fdb
yamakou <yamakou@example.com>
2014-12-31 17:19:46 +0900

    Finish static page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -8,6 +8,10 @@ describe "Static pages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+		it "should have the right title" do
+			  visit '/static_pages/home'
+			  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Home")
+		end
   end
 
   describe "Help page" do
@@ -16,6 +20,10 @@ describe "Static pages" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
+		it "should have the right title" do
+			  visit '/static_pages/help'
+			  expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
+		end
   end
 
 	describe "About page" do
@@ -23,6 +31,10 @@ describe "Static pages" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
     end
+		it "should have the right title" do
+			  visit '/static_pages/about'
+			  expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+		end
 	end
 
 end
