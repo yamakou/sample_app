1d64b325109a0d7d2ac6071f3f1fb1d7e06443eb
yamakou <yamakou@example.com>
2015-01-12 12:38:01 +0900

    Add user show view.

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -3,11 +3,20 @@ require 'spec_helper'
 describe "UserPages" do
 	subject {page}
 
-	describe "signup page" do
-	before { visit signup_path}
+	describe "profile page" do
+		let(:user) { FactoryGirl.create(:user) }
+		before { visit user_path(user) }
 
-	it { should have_content('Sign up')}
-	it { should have_title(full_title('Sign up'))}
+		it { should have_content(user.name) }
+		it { should have_title(user.name) }
 	end
+
+	describe "signup page" do
+		before { visit signup_path}
+
+		it { should have_content('Sign up')}
+		it { should have_title(full_title('Sign up'))}
+	end
+
 
 end
