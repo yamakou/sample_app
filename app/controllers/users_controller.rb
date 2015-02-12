db54229bd5ef4cf61953b1af5571dc70a2cd9e03
yamakou <yamakou@example.com>
2015-02-12 11:35:24 +0900

    Finish sign in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,8 @@ class UsersController < ApplicationController
 		end
 	end
 
+	private
+
 	def user_params
 		params.require(:user).permit(:name, :email, :password, :password_confirmation)
 	end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -12,7 +12,10 @@ describe User do
 	it { should respond_to(:password_digest) }
 	it { should respond_to(:password) }
 	it { should respond_to(:password_confirmation) }
+	it { should respond_to(:remember_token) }
 	it { should respond_to(:authenticate) }
+
+
 
 	it { should be_valid }
 
@@ -26,6 +29,10 @@ describe User do
 		it {should_not be_valid}
 	end
 
+	  describe "remember token" do
+			    before { @user.save }
+					    its(:remember_token) { should_not be_blank }
+							  end
 
 	describe "when name is too long" do
 		before { @user.name = "a"*51}
@@ -106,4 +113,10 @@ describe User do
     end
 	end
 
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+
+	end
 end
